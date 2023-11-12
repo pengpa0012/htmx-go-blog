71dbf26ce8686e516e1566065b32185f56db9426
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-12 22:27:41 +0800

    added boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+	"github.com/labstack/echo/v4"
+	// "math/rand"
+	"html/template"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Home(c echo.Context) error {
+	return c.Render(http.StatusOK, "index.html", "/")
+}
+
 
 func main() {
-	fmt.Println("Hello World")
+	e := echo.New()
+
+	t := &Template{
+			templates: template.Must(template.ParseGlob("web/templates/*.html")),
+	}
+
+	e.Renderer = t
+	e.GET("/", Home)
+	e.Logger.Fatal(e.Start(":5000"))
 }
