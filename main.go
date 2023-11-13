96b9dfc6700bc6c6c48628a22bea881093b058cb
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-14 07:37:26 +0800

    update template, added sample data

--- main.go
+++ main.go
@@ -12,6 +12,19 @@ type Template struct {
 	templates *template.Template
 }
 
+type Blog struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	DateCreated string `json:"date_created"`
+}
+
+var blogs = []Blog {
+	{ID: "1", Title: "Title 1", Description: "Description 1", DateCreated: "DateCreated 1"},
+	{ID: "2", Title: "Title 2", Description: "Description 2", DateCreated: "DateCreated 2"},
+	{ID: "3", Title: "Title 3", Description: "Description 3", DateCreated: "DateCreated 3"},
+}
+
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
@@ -19,6 +32,25 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 func Home(c echo.Context) error {
 	return c.Render(http.StatusOK, "index.html", "/")
 }
+
+func getBlogs(c echo.Context) error {
+	return c.Render(http.StatusOK, "cards.html", blogs)
+}
+
+func addBlog(c echo.Context) error {
+	return c.Render(http.StatusOK, "cards.html", nil)
+}
+
+func removeBlog(c echo.Context) error {
+	return c.Render(http.StatusOK, "cards.html", nil)
+}
+
+func updateBlog(c echo.Context) error {
+	return c.Render(http.StatusOK, "cards.html", nil)
+}
+
+
+
 
 
 func main() {
@@ -30,5 +62,6 @@ func main() {
 
 	e.Renderer = t
 	e.GET("/", Home)
+	e.GET("/blogs", getBlogs)
 	e.Logger.Fatal(e.Start(":5000"))
 }
